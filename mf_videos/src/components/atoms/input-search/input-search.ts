6c1421b7967ce4c5178f95de3911b0d08f0d7fca
Anderson Nascimento <anderson.nascimento@meta.com.br>
2024-07-02 17:58:56 -0300

    chore: add input search component

---
+++ mf_videos/src/components/atoms/input-search/input-search.ts
@@ -0,0 +1,31 @@
+class InputSearch extends HTMLElement {
+  constructor() {
+    super()
+    this.attachShadow({ mode: 'open', delegatesFocus: true });
+  }
+
+  async connectedCallback() {
+    const style = `
+      .search-video {
+        color: rgb(33, 33, 33);
+        font-size: 1rem;
+        padding: 0.5rem;
+        margin-bottom: 1rem;
+        width: 50%;
+      }
+  `
+
+    const template = `
+      <style>${style}</style>
+      <input type="text" id="search-video" class="search-video" placeholder="Pesquisar por nome">
+    `
+
+    this.shadowRoot!.innerHTML = template
+  }
+}
+
+if (!customElements.get('input-search')) {
+  customElements.define('input-search', InputSearch)
+}
+
+export default InputSearch
