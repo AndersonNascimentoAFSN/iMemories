c503b6dc8a60cd4fef24532b175136b7a3b45b23
Anderson Nascimento <anderson.nascimento@meta.com.br>
2024-07-03 16:28:49 -0300

    feat: create db

--- mf_videos/src/services/videos.ts
+++ mf_videos/src/services/videos.ts
@@ -1,25 +1,8 @@
 import { IVideo } from "../../types/video";
 
-export async function getVideos(id?: string) {
-  try {
-    const movies: Promise<IVideo[]> = fetch(`http://localhost:3333/videos/${id ?? ''}`)
-      .then(response => response.json())
-      .then(data => {
-        return data
-      }).catch(err => {
-        console.error(err.message)
-        return null
-      })
-
-    return movies
-  } catch {
-    null
-  }
-}
-
-// export async function getVideos(query?: { name?: string, maxResults?: number }) {
+// export async function getVideos(id?: string) {
 //   try {
-//     const movies: Promise<IVideo[]> = fetch(`http://localhost:3333/videos?q=${query?.name ? query?.name : ''}&maxResults=${query?.maxResults ? query?.maxResults : 8}`)
+//     const movies: Promise<IVideo[]> = fetch(`http://localhost:3333/videos/${id ?? ''}`)
 //       .then(response => response.json())
 //       .then(data => {
 //         return data
@@ -34,58 +17,19 @@ export async function getVideos(id?: string) {
 //   }
 // }
 
-// export async function getFavoritesVideos() {
-//   try {
-//     const movies: Promise<IVideo[]> = fetch('http://localhost:3333/favorites').then(response => response.json()).then(data => {
-//       return data
-//     })
+export async function getVideos(query?: { name?: string, maxResults?: number }) {
+  try {
+    const movies: Promise<IVideo[]> = fetch(`http://localhost:3333/videos?q=${query?.name ? query?.name : ''}&maxResults=${query?.maxResults ? query?.maxResults : 8}`)
+      .then(response => response.json())
+      .then(data => {
+        return data
+      }).catch(err => {
+        console.error(err.message)
+        return null
+      })
 
-//     return movies
-//   } catch {
-//     null
-//   }
-// }
-
-// export async function createFavoritesVideos({
-//   thumbnail,
-//   title,
-//   videoId
-// }: {
-//   videoId: string
-//   title: string
-//   thumbnail: string
-// }) {
-//   try {
-//     const movies: Promise<IVideo[]> = fetch('http://localhost:3333/favorites', {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json'
-//       },
-//       body: JSON.stringify({
-//         videoId: videoId,
-//         title: title,
-//         thumbnail: thumbnail,
-//       })
-//     }).then(response => response.json()).then(data => {
-//       return data
-//     })
-
-//     return movies
-//   } catch {
-//     null
-//   }
-
-// }
-
-// export async function getVideoById(id: string | undefined) {
-//   try {
-//     const movie: Promise<IVideo> = fetch(`http://localhost:3333/favorites/${id}`).then(response => response.json()).then(data => {
-//       return data
-//     })
-
-//     return movie
-//   } catch {
-//     null
-//   }
-// }
-
+    return movies
+  } catch {
+    null
+  }
+}
