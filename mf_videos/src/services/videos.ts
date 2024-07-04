7bfcb1b671f9245bfeba26210a5b4609f5452405
Anderson Nascimento <anderson.nascimento@meta.com.br>
2024-07-04 13:59:37 -0300

    fix: build to compose

--- mf_videos/src/services/videos.ts
+++ mf_videos/src/services/videos.ts
@@ -1,8 +1,9 @@
 import { IVideo } from "../../types/video";
 
+const API_BFF = process.env.API_BFF
 export async function getVideos(query?: { name?: string, maxResults?: number }) {
   try {
-    const movies: Promise<IVideo[]> = fetch(`http://localhost:3003/videos?q=${query?.name ? query?.name : ''}&maxResults=${query?.maxResults ? query?.maxResults : 8}`)
+    const movies: Promise<IVideo[]> = fetch(`${API_BFF}/videos?q=${query?.name ? query?.name : ''}&maxResults=${query?.maxResults ? query?.maxResults : 8}`)
       .then(response => response.json())
       .then(data => {
         return data
